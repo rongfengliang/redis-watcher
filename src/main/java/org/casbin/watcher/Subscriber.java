af8aab2778c159aa1b0ef1aab1b02601fff23136
rong fengliang <1141591465@qq.com>
2021-06-23 09:23:53 +0800

    merge: merge from callback

--- src/main/java/org/casbin/watcher/Subscriber.java
+++ src/main/java/org/casbin/watcher/Subscriber.java
@@ -3,28 +3,59 @@ package org.casbin.watcher;
 import redis.clients.jedis.JedisPubSub;
 
 import java.util.Objects;
+import java.util.function.Consumer;
 
 public class Subscriber extends JedisPubSub {
     private Runnable runnable;
-    private ChannelRunnable channelRunnable; // for path chananel name;
+    private Consumer<String> consumer;
+    private Boolean ignoreSelf;
+    private String localid;
     public Subscriber(Runnable updateCallback) {
         this.runnable = updateCallback;
     }
-    public Subscriber(Runnable updateCallback,ChannelRunnable channelRunnable) {
+    public Subscriber(Runnable updateCallback,Consumer<String> consumer,Boolean ignoreSelf,String localid) {
         this.runnable = updateCallback;
-        this.channelRunnable=channelRunnable;
+        this.consumer=consumer;
+        this.ignoreSelf=ignoreSelf;
+        this.localid=localid;
     }
-
+    public Subscriber(Runnable updateCallback,Boolean ignoreSelf,String localid) {
+        this.runnable = updateCallback;
+        this.ignoreSelf=ignoreSelf;
+        this.localid=localid;
+    }
+    public Subscriber(Consumer<String> consumer,Boolean ignoreSelf,String localid) {
+        this.consumer = consumer;
+        this.ignoreSelf=ignoreSelf;
+        this.localid=localid;
+    }
     public void setUpdateCallback(Runnable runnable){
         this.runnable = runnable;
     }
 
+    public void setUpdateCallback(Consumer<String> func){
+        this.consumer = func;
+    }
     public void onMessage(String channel, String message) {
-        runnable.run();
-        /* for patch offical not contains  channal name */
-        if(Objects.nonNull(channelRunnable)) {
-            this.channelRunnable.setChannelName(channel);
-            this.channelRunnable.run();
+        if(ignoreSelf){
+            // do nothing  with local update
+            if(!message.contains(this.localid)){
+                // with message
+                messageProcess(channel, message);
+            }
+        }else{
+            messageProcess(channel, message);
+        }
+    }
+
+    private void messageProcess(String channel, String message) {
+        if(Objects.nonNull(consumer)) {
+            consumer.accept(message);
+        }else{
+            // other with runable
+            if(Objects.nonNull(runnable)) {
+                runnable.run();
+            }
         }
     }
 }
